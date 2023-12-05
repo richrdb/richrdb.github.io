861e1a0e4da2f6c7257f84d446afce917b5f9c7a
Richard <82099665+richrdb@users.noreply.github.com>
2023-12-05 14:40:20 +0100

    Delete src directory

--- src/js/nav.js
+++
@@ -1,8 +0,0 @@
-/* Listenner and Transparency Applicator */
-$(window).scroll(function () {
-if ($(window).scrollTop() >= 50) {
-$('nav').css('background','#212121');
-} else {
-$('nav').css('background','transparent');
-}
-});
